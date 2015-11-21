55513245f0b970401b31f4ed8896a3285a78cb75
MichaelFan <fanlinsheng@gmail.com>
2015-11-21 15:58:58 +0800

    Draw a triangle with GLSL

---
+++ include/shader.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Michael.Fan on 11/21/15.
+//
+
+#ifndef MINE_SWEEPER_SHADER_H
+#define MINE_SWEEPER_SHADER_H
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif //MINE_SWEEPER_SHADER_H
