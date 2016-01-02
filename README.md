8857eff4a467fb24501c8cb425b8086a0f84a09e
MichaelFan <fanlinsheng@gmail.com>
2016-01-02 14:00:55 +0800

    Apply MVP

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mine-sweeper
 MicroSoft classic game
+
+## Requirements
+* CMake 3.0
+* OpenGL 3.3
+* GLFW 3 
+
+## Build && Run (Mac OSX)
+```
+mkdir build && cd build
+cmake .. && make
+cd ..
+./build/mine-sweeper
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Include standard headers
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -10,8 +11,9 @@
 GLFWwindow* window;
 
 // Include GLM
-//#include <glm/glm.hpp>
-//using namespace glm;
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
 
 #include <shader.hpp>
 
@@ -29,7 +31,7 @@ int main( void )
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
 
 	// Open a window and create its OpenGL context
 	window = glfwCreateWindow(1024, 768, "mine-sweeper", NULL, NULL);
@@ -61,14 +63,31 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "shader/main.vs.glsl", "shader/main.fs.glsl" );
+	GLuint programID = LoadShaders("shader/main.vs.glsl", "shader/main.fs.glsl");
 
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix : 45 degree Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+			glm::vec3(4,4,3), // Camera is at (4,3,3), in World Space
+			glm::vec3(0,0,0), // and looks at the origin
+			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model      = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
 	static const GLfloat g_vertex_buffer_data[] = {
 			-1.0f, -1.0f, 0.0f,
 			1.0f, -1.0f, 0.0f,
 			0.0f,  1.0f, 0.0f,
 	};
+//	static const GLushort g_element_buffer_data[] = { 0, 1, 2 };
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
@@ -83,11 +102,15 @@ int main( void )
 		// Use our shader
 		glUseProgram(programID);
 
+		// Send our transformation to the currently bound shader,
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+				0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
 				3,                  // size
 				GL_FLOAT,           // type
 				GL_FALSE,           // normalized?
@@ -108,10 +131,10 @@ int main( void )
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 				 glfwWindowShouldClose(window) == 0 );
 
-	// Cleanup VBO
+	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
 	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
